5cbf91594042872506e77f013ce3d5b9774ca626
Koruji <jadecottin212@gmail.com>
2025-02-05 15:20:56 +0100

    GET /trips

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
+const Trips = require("../models/trips");
 var express = require('express');
 var router = express.Router();
 
-const Trips = require("../models/trips");
 
+router.get("/trips", async(req, res) => {
+  try {
 
+    const data = await Trips.find();
+    res.status(201).json({result: true, trips: data});
+
+  } catch(error) {
+    res.status(404).json({result: false, message: "Trips not found"});
+  }
+  
+});
 
 
 module.exports = router;
