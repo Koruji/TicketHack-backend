92e6713d1d619eaa6ed27d7ffb1c7b758e8a792b
Koruji <jadecottin212@gmail.com>
2025-02-06 10:41:11 +0100

    Modify POST /purchase Modify GET /purchase Add description

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,16 @@ const moment = require("moment");
 var express = require('express');
 var router = express.Router();
 
-
+//GET /trips 
+//Résulat attendu : 
+// result : true,
+// trips : [{
+//      arrival: "Lyon"
+//      date: "2025-02-06T00:01:21.013Z"
+//      departure: "Marseille"
+//      price: 60
+//      _id: "67a3700b6c009280a252cc37"
+//  }]
 router.get("/trips", async(req, res) => {
   try {
 
@@ -31,6 +40,11 @@ router.get("/trips", async(req, res) => {
   }
 });
 
+//POST /book/:id 
+//Résulat attendu : 
+// result: true,
+// message: "Trip saved !"
+//tranfert un voyage de la collection trips vers la collection carts
 router.post("/book/:id", async(req, res) => {
   try {
 
@@ -62,6 +76,16 @@ router.post("/book/:id", async(req, res) => {
   }
 });
 
+//GET /cart 
+//Résulat attendu : 
+// result : true,
+// trips : [{
+//      arrival: "Lyon"
+//      date: "2025-02-06T00:01:21.013Z"
+//      departure: "Marseille"
+//      price: 60
+//      _id: "67a3700b6c009280a252cc37"
+//  }]
 router.get("/cart", async (req, res) => {
   try {
     
@@ -78,36 +102,64 @@ router.get("/cart", async (req, res) => {
   }
 });
 
+//GET /purchase 
+//Résulat attendu : 
+// result : true,
+// purchases : [{
+//      arrival: "Lyon"
+//      date: "2025-02-06T00:01:21.013Z"
+//      departure: "Marseille"
+//      price: 60
+//      _id: "67a3700b6c009280a252cc37"
+//  }]
 router.get("/purchase", async (req, res) => {
   try {
     const data = await Purchase.find();
-    res.status(200).json({ result: true, purchases: data });
+    
+    if(data.length === 0) {
+      res.status(404).json({result: false, message: "Trips not found"});
+    } else {
+      res.status(201).json({result: true, trips: data});
+    }
+    
   } catch (error) {
     res.status(404).json({ result: false, message: "Purchases not found" });
   }
 });
 
-router.post("/purchase/:id", async (req, res) => {
+//POST /purchase 
+//Résulat attendu : 
+// result : true,
+// message: "Purchase completed!",
+// purchase : [{
+//      arrival: "Lyon"
+//      date: "2025-02-06T00:01:21.013Z"
+//      departure: "Marseille"
+//      price: 60
+//      _id: "67a3700b6c009280a252cc37"
+//  }]
+router.post("/purchase", async (req, res) => {
   try {
-    const purchaseData = await Purchase.findById(req.params.id);
+    const data = await Cart.find();
 
-    console.log(purchaseData);
+    console.log(data);
 
-    if (purchaseData) {
-      const newPurchase = new Purchase({
-        departure: purchaseData.departure,
-        arrival: purchaseData.arrival,
-        date: purchaseData.date,
-        price: purchaseData.price,
-      });
-
-      const savedPurchase = await newPurchase.save();
-
-      await Purchase.deleteOne({ _id: purchaseData._id });
-
+    if (data.length !== 0) {
+      for(const purchaseData of data) {
+        const newPurchase = new Purchase({
+          departure: purchaseData.departure,
+          arrival: purchaseData.arrival,
+          date: purchaseData.date,
+          price: purchaseData.price,
+        });
+  
+        await newPurchase.save();
+  
+        await Cart.deleteOne({ _id: purchaseData._id });
+      }
       res
         .status(201)
-        .json({ result: true, message: "Purchase completed!", purchase: savedPurchase });
+        .json({ result: true, message: "Purchase completed!", purchase: data });
     } else {
       res
         .status(404)
